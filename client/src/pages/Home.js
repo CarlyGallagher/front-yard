859cb7f142e000403023cff9e487c76db0fc764f
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-06-06 18:56:15 -0700

    DRY

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_LISTINGS } from "../utils/queries";
 import Signup from "./Signup";
 import Login from "./Login";
 import { loggedIn, logout } from "../utils/auth";
@@ -8,8 +6,7 @@ import { loggedIn, logout } from "../utils/auth";
 
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_LISTINGS);
-  const listing = data?.listings || [];
+
   return <main>
     {!loggedIn() ? (
       <>
