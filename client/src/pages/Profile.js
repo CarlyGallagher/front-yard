e4b3a58b9dcc9fea1cc213f8bd6e24718a89ae46
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-06-10 22:50:55 -0700

    late night quick commit/profile

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";  
+import "./profile.css";
+
+const Profile = () => {
+    return (<main>
+        <div>
+            <h3>This is a Profiles bones</h3>
+        </div>
+    </main>
+    );
+};
+
+export default Profile;
